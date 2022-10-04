6678416d049a8e222875901630e69f33131dfaf7
mnmt7 <107614965+mnmt7@users.noreply.github.com>
2022-10-04 20:20:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,17 +2,15 @@ function randomRange(min, max) {
     return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-function generateList (size, minLen, maxLen){
+function generateList (size, minLen, maxLen) {
     let newList = [];
-    let i = 0, elementsInAGroup = (Math.floor(size/4));
+    let i = 0;
     while (i < size) {
         const len = randomRange(minLen, maxLen);   
         const bar = document.createElement('div');
-        
-        // bar.className = "neon" + (Math.floor(i/elementsInAGroup));
+
         bar.className = "bar";
-        bar.style.height = len + "px";
-        bar.id = Math.floor(i/elementsInAGroup);
+        bar.style.height = len;
         
         newList.push({
             val: len,
@@ -28,8 +26,7 @@ function generateList (size, minLen, maxLen){
 function pause(time) {
     let p = new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve("hi");
-            console.log("inside pause");
+            resolve();
         }, time * 1000)
     })
     return p;
@@ -43,29 +40,117 @@ function render() {
     });
 }
 
-function incr() {
-    delay -= 0.4 * delay;
-    console.log(delay);
+function increaseSpeed() {
+    if (delay <= 0.0125) {
+        document.getElementById("increase-speed-btn").disabled = true;
+        document.getElementById("increase-speed-btn").style.opacity = 0.5;
+        return;
+    }
+    
+    delay /= 2;
+    
+    document.getElementById("decrease-speed-btn").disabled = false;
+    document.getElementById("decrease-speed-btn").style.opacity = 1;
 }
 
-function decr() {
-    delay += 0.4 * delay;
-    console.log(delay);
+function decreaseSpeed() {
+    if (delay >= 0.8) {
+        document.getElementById("decrease-speed-btn").disabled = true;
+        document.getElementById("decrease-speed-btn").style.opacity = 0.5;
+        return;        
+    }
+    
+    delay *= 2;
+
+    document.getElementById("increase-speed-btn").disabled = false;
+    document.getElementById("increase-speed-btn").style.opacity = 1;
 }
 
-document.getElementById("merge-sort-btn").addEventListener("click", () => { mergeSort(0, list.length - 1); console.log("hey me again")});
-document.getElementById("selection-sort-btn").addEventListener("click", () => selectionSort());
-document.getElementById("bubble-sort-btn").addEventListener("click", () => bubbleSort());
-document.getElementById("insertion-sort-btn").addEventListener("click", () => insertionSort());
+function increaseSize() {
+    if (size >= 70) {
+        document.getElementById("increase-size-btn").disabled = true;
+        document.getElementById("increase-size-btn").style.opacity = 0.5;
+        return;
+    }
 
-document.getElementById("incr").addEventListener("click", incr);
-document.getElementById("decr").addEventListener("click", decr);
+    document.getElementById("decrease-size-btn").disabled = false;
+    document.getElementById("decrease-size-btn").style.opacity = 1;
 
+    size += 5;
+    generateList(size, minLen, maxLen);
+}
+
+function decreaseSize() {
+    if (size <= 40) {
+        document.getElementById("decrease-size-btn").disabled = true;
+        document.getElementById("decrease-size-btn").style.opacity = 0.5;
+        return;        
+    }
+
+    document.getElementById("increase-size-btn").disabled = false;
+    document.getElementById("increase-size-btn").style.opacity = 1;
+
+    size -= 5;
+    generateList(size, minLen, maxLen);
+}
+
+async function sort(event) {
+    const id = event.target.id;
+
+    disableButtons(true);
+
+    if (id === "merge-sort-btn")
+        await(mergeSort(0, size - 1));
+    else if (id === "selection-sort-btn")
+        await(selectionSort());
+    else if (id === "bubble-sort-btn")
+        await(bubbleSort());
+    else if (id === "quick-sort-btn")
+        await(quickSort(0, size - 1));
+    else if (id === "heap-sort-btn")
+        await(heapSort(size));
+    else
+        await(insertionSort());
+
+    disableButtons(false);
+}
+
+function disableButtons(flag) {
+    document.getElementById("merge-sort-btn").disabled = flag;
+    document.getElementById("selection-sort-btn").disabled = flag;
+    document.getElementById("bubble-sort-btn").disabled = flag;
+    document.getElementById("insertion-sort-btn").disabled = flag;
+    document.getElementById("quick-sort-btn").disabled = flag;
+    document.getElementById("heap-sort-btn").disabled = flag;
+    document.getElementById("increase-size-btn").disabled = flag;
+    document.getElementById("decrease-size-btn").disabled = flag;
+    document.getElementById("generate-new-array").disabled = flag;
+    
+    document.getElementById("merge-sort-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("selection-sort-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("bubble-sort-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("insertion-sort-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("quick-sort-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("heap-sort-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("increase-size-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("decrease-size-btn").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("generate-new-array").style.opacity = flag ? 0.5 : 1;
+    document.getElementById("size-label").style.opacity = flag ? 0.5 : 1;
+}
+
+document.getElementById("merge-sort-btn").addEventListener("click", (event) => sort(event));
+document.getElementById("selection-sort-btn").addEventListener("click", (event) => sort(event));
+document.getElementById("bubble-sort-btn").addEventListener("click", (event) => sort(event));
+document.getElementById("insertion-sort-btn").addEventListener("click", (event) => sort(event));
+document.getElementById("quick-sort-btn").addEventListener("click", (event) => sort(event));
+document.getElementById("heap-sort-btn").addEventListener("click", (event) => sort(event));
+document.getElementById("increase-speed-btn").addEventListener("click", increaseSpeed);
+document.getElementById("decrease-speed-btn").addEventListener("click", decreaseSpeed);
+document.getElementById("increase-size-btn").addEventListener("click", increaseSize);
+document.getElementById("decrease-size-btn").addEventListener("click", decreaseSize);
 document.getElementById("generate-new-array").addEventListener("click", () => generateList(size, minLen, maxLen));
 
-// size % noOfGroups must be 0
-let size = 60, minLen = 20, maxLen = 400, noOfGroups = 4, elementsInAGroup = (Math.floor(size/noOfGroups));
+let size = 60, minLen = 20, maxLen = 350;
 let list = [];
 generateList(size, minLen, maxLen);
 let delay = 0.1;
-console.log(list)
